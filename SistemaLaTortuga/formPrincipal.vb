504ef07a057a53895d18d7e8a917fe92e042e766
matiadr <mati.d@live.com.ar>
2014-08-17 14:03:57 -0300

    creo formulario clientes

--- SistemaLaTortuga/formPrincipal.vb
+++ SistemaLaTortuga/formPrincipal.vb
@@ -1,3 +1,6 @@
 ﻿Public Class formPrincipal
 
+    Private Sub ClientesToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClientesToolStripMenuItem.Click
+        formClientes.Show()
+    End Sub
 End Class
