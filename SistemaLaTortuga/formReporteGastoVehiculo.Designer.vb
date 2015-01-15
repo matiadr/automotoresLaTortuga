44e26bcd4dfa08d952c4f12ea97f14780e9fc355
matiadr <mati.d@live.com.ar>
2015-01-14 22:21:50 -0300

    cambios

---
+++ SistemaLaTortuga/formReporteGastoVehiculo.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class formReporteGastoVehiculo
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.textIdVeh = New System.Windows.Forms.TextBox()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.tbBusqueda = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.dgVehiculos = New System.Windows.Forms.DataGridView()
+        Me.idVehiculo = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.marca = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.modelo = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.tipoVehiculo = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.año = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.dominio = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.idcliente = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.dgVehiculos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.textIdVeh)
+        Me.GroupBox3.Controls.Add(Me.label1)
+        Me.GroupBox3.Controls.Add(Me.tbBusqueda)
+        Me.GroupBox3.Location = New System.Drawing.Point(16, 9)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(723, 42)
+        Me.GroupBox3.TabIndex = 14
+        Me.GroupBox3.TabStop = False
+        '
+        'textIdVeh
+        '
+        Me.textIdVeh.Location = New System.Drawing.Point(638, 16)
+        Me.textIdVeh.Name = "textIdVeh"
+        Me.textIdVeh.Size = New System.Drawing.Size(37, 20)
+        Me.textIdVeh.TabIndex = 11
+        Me.textIdVeh.Visible = False
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Location = New System.Drawing.Point(223, 19)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(40, 13)
+        Me.label1.TabIndex = 10
+        Me.label1.Text = "Buscar"
+        '
+        'tbBusqueda
+        '
+        Me.tbBusqueda.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.tbBusqueda.Location = New System.Drawing.Point(282, 16)
+        Me.tbBusqueda.Name = "tbBusqueda"
+        Me.tbBusqueda.Size = New System.Drawing.Size(188, 20)
+        Me.tbBusqueda.TabIndex = 9
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.dgVehiculos)
+        Me.GroupBox2.Location = New System.Drawing.Point(15, 51)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(724, 331)
+        Me.GroupBox2.TabIndex = 13
+        Me.GroupBox2.TabStop = False
+        '
+        'dgVehiculos
+        '
+        Me.dgVehiculos.AllowUserToAddRows = False
+        Me.dgVehiculos.AllowUserToDeleteRows = False
+        Me.dgVehiculos.AllowUserToResizeRows = False
+        Me.dgVehiculos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgVehiculos.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.idVehiculo, Me.marca, Me.modelo, Me.tipoVehiculo, Me.año, Me.dominio, Me.idcliente})
+        Me.dgVehiculos.Location = New System.Drawing.Point(6, 19)
+        Me.dgVehiculos.Name = "dgVehiculos"
+        Me.dgVehiculos.ReadOnly = True
+        Me.dgVehiculos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgVehiculos.Size = New System.Drawing.Size(702, 306)
+        Me.dgVehiculos.TabIndex = 5
+        Me.dgVehiculos.TabStop = False
+        '
+        'idVehiculo
+        '
+        Me.idVehiculo.DataPropertyName = "IdVehiculo"
+        Me.idVehiculo.HeaderText = "idVehiculo"
+        Me.idVehiculo.Name = "idVehiculo"
+        Me.idVehiculo.ReadOnly = True
+        Me.idVehiculo.Visible = False
+        '
+        'marca
+        '
+        Me.marca.DataPropertyName = "NombreMarca"
+        Me.marca.HeaderText = "Marca"
+        Me.marca.Name = "marca"
+        Me.marca.ReadOnly = True
+        Me.marca.Width = 150
+        '
+        'modelo
+        '
+        Me.modelo.DataPropertyName = "NombreModelo"
+        Me.modelo.HeaderText = "Modelo"
+        Me.modelo.Name = "modelo"
+        Me.modelo.ReadOnly = True
+        Me.modelo.Width = 150
+        '
+        'tipoVehiculo
+        '
+        Me.tipoVehiculo.DataPropertyName = "Tipo"
+        Me.tipoVehiculo.HeaderText = "Tipo Vehiculo"
+        Me.tipoVehiculo.Name = "tipoVehiculo"
+        Me.tipoVehiculo.ReadOnly = True
+        Me.tipoVehiculo.Width = 150
+        '
+        'año
+        '
+        Me.año.DataPropertyName = "Año"
+        Me.año.HeaderText = "Año"
+        Me.año.Name = "año"
+        Me.año.ReadOnly = True
+        Me.año.Width = 70
+        '
+        'dominio
+        '
+        Me.dominio.DataPropertyName = "Dominio"
+        Me.dominio.HeaderText = "Dominio"
+        Me.dominio.Name = "dominio"
+        Me.dominio.ReadOnly = True
+        '
+        'idcliente
+        '
+        Me.idcliente.DataPropertyName = "IdCliente"
+        Me.idcliente.HeaderText = "idcliente"
+        Me.idcliente.Name = "idcliente"
+        Me.idcliente.ReadOnly = True
+        Me.idcliente.Visible = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Location = New System.Drawing.Point(17, 382)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(722, 71)
+        Me.GroupBox1.TabIndex = 12
+        Me.GroupBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Image = Global.SistemaLaTortuga.My.Resources.Resources.edit
+        Me.Button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button2.Location = New System.Drawing.Point(253, 19)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(91, 34)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Generar Reporte"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Image = Global.SistemaLaTortuga.My.Resources.Resources._exit
+        Me.Button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button1.Location = New System.Drawing.Point(379, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(80, 34)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Salir"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'formReporteGastoVehiculo
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(745, 462)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.Name = "formReporteGastoVehiculo"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Seleccionar Auto"
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        CType(Me.dgVehiculos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents textIdVeh As System.Windows.Forms.TextBox
+    Friend WithEvents label1 As System.Windows.Forms.Label
+    Friend WithEvents tbBusqueda As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents dgVehiculos As System.Windows.Forms.DataGridView
+    Friend WithEvents idVehiculo As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents marca As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents modelo As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents tipoVehiculo As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents año As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents dominio As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents idcliente As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+End Class
---
+++ SistemaLaTortuga/formReporteGastoVehiculo.vb
@@ -0,0 +1,66 @@
+﻿Imports System.Data.SqlClient
+Public Class formReporteGastoVehiculo
+
+    Private Sub tbBusqueda_TextChanged(sender As Object, e As EventArgs) Handles tbBusqueda.TextChanged
+        Dim CN As String = "Data Source='" & formPrincipal.tbEquipo.Text & "';INITIAL Catalog='" & formPrincipal.tbBSD.Text & "' ;Persist Security Info=True;User ID='" & formPrincipal.tbUsuario.Text & "';Password='" & formPrincipal.tbClave.Text & "'"
+        Dim miConexion As New SqlConnection(CN)
+            Dim seleccion As String = "select IdVehiculo,NombreMarca,NombreModelo,Tipo,Año,Dominio from Vehiculos v,Modelos m,Marcas ma where v.IdModelo=m.IdModelo and m.IdMarca=ma.IdMarca and (NombreMarca like '" & tbBusqueda.Text & "%' or NombreModelo like '" & tbBusqueda.Text & "%' or Dominio like '" & tbBusqueda.Text & "%') and vendido = 'N' order by NombreMarca,NombreModelo"
+            miConexion.Open()
+            Dim tabla2 As DataTable
+            Dim da As SqlDataAdapter
+            da = New SqlDataAdapter(seleccion, CN)
+            tabla2 = New DataTable
+            da.Fill(tabla2)
+            dgVehiculos.DataSource = tabla2
+        miConexion.Close()
+    End Sub
+
+    Private Sub cargarDGVehiculos()
+        Dim CN As New SqlConnection("Data Source='" & formPrincipal.tbEquipo.Text & "';INITIAL Catalog='" & formPrincipal.tbBSD.Text & "' ;Persist Security Info=True;User ID='" & formPrincipal.tbUsuario.Text & "';Password='" & formPrincipal.tbClave.Text & "'")
+        CN.Open()
+        Dim cmd As SqlCommand
+        cmd = New SqlCommand("select IdVehiculo,NombreMarca,NombreModelo,Tipo,Año,Dominio from Vehiculos v,Modelos m,Marcas ma where v.IdModelo=m.IdModelo and m.IdMarca=ma.IdMarca and Vendido = 'N' order by NombreMarca,NombreModelo", CN)
+        Dim lista As SqlDataReader = cmd.ExecuteReader
+        Dim dt As New DataTable()
+        dt.Load(lista)
+        dgVehiculos.DataSource = dt
+        CN.Close()
+    End Sub
+
+    Private Sub formReporteGastoVehiculo_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cargarDGVehiculos()
+    End Sub
+
+
+    Private Sub dgVehiculos_Click(sender As Object, e As EventArgs) Handles dgVehiculos.Click
+        If dgVehiculos.RowCount > 0 Then
+            textIdVeh.Text = dgVehiculos.Item("idVehiculo", dgVehiculos.SelectedRows(0).Index).Value()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If textIdVeh.Text = "" Then
+            MessageBox.Show("Seleccione un auto antes de generar el reporte", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+        Dim CN As New SqlConnection("Data Source='" & formPrincipal.tbEquipo.Text & "';INITIAL Catalog='" & formPrincipal.tbBSD.Text & "' ;Persist Security Info=True;User ID='" & formPrincipal.tbUsuario.Text & "';Password='" & formPrincipal.tbClave.Text & "'")
+        Dim daDatos As New SqlDataAdapter  ' Objeto Adaptador para leer datos de la Base de datos
+        Dim dtDatos As New DataSet  ' datatable para recibir los datos de la base de datos
+        CN.Open()
+        Dim seleccion As String = "select FechaGasto as fecha,DetalleGasto as detalle,ImporteGasto as importe,NombreProveedor as proveedor from GastosVehiculos g, Proveedores p, VehiculosXClientes vxc where g.IdProveedor = p.IdProveedor and g.IdVxC = vxc.IdVxC and vxc.IdVehiculo = '" & textIdVeh.Text & "'"
+        daDatos = New SqlDataAdapter(seleccion, CN)
+        daDatos.Fill(dtDatos, "GastosXVehiculo")
+        CN.Close()
+        Dim rpt As reporteGastosXVehiculo = New reporteGastosXVehiculo
+        rpt.SetDataSource(dtDatos)
+        Dim vehiculo As String = dgVehiculos.Item("marca", dgVehiculos.SelectedRows(0).Index).Value() + " " + dgVehiculos.Item("modelo", dgVehiculos.SelectedRows(0).Index).Value() + " " + dgVehiculos.Item("tipoVehiculo", dgVehiculos.SelectedRows(0).Index).Value() + " " + Conversion.Str(dgVehiculos.Item("año", dgVehiculos.SelectedRows(0).Index).Value()) + ", Dominio: " + dgVehiculos.Item("dominio", dgVehiculos.SelectedRows(0).Index).Value()
+        rpt.SetParameterValue("vehiculo", vehiculo)
+        FormReporteGastosXVehiculo.viewerGastosXVehiculos.ReportSource = rpt
+
+        FormReporteGastosXVehiculo.ShowDialog()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+End Class
